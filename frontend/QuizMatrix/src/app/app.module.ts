621b295b7cb9c2c35e29425a083ffda9396ce17c
Mari <axana-marinela.diaconu@student.tuiasi.ro>
2023-11-14 01:23:08 +0200

    Add header component

--- frontend/QuizMatrix/src/app/app.module.ts
+++ frontend/QuizMatrix/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideClientHydration()
