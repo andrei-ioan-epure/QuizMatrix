749874c6a183d321cc1ed9418ab1c9f4815e2d65
Mari <axana-marinela.diaconu@student.tuiasi.ro>
2023-11-27 19:49:01 +0200

    Add routes and edit colors

--- frontend/QuizMatrix/src/app/teste-adaugate/teste-adaugate.component.ts
+++ frontend/QuizMatrix/src/app/teste-adaugate/teste-adaugate.component.ts
@@ -3,14 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-teste-adaugate',
   templateUrl: './teste-adaugate.component.html',
-  styleUrl: './teste-adaugate.component.css'
+  styleUrl: './teste-adaugate.component.css',
 })
 export class TesteAdaugateComponent {
-
   teste = [
-    { nume: 'Test 1', scor: 85, durata:'00:10:15', domeniu:'Biologie',icon: '../../assets/images/biology.png' },
-    { nume: 'Test 2', scor: 92, durata:'00:7:20', domeniu:'Matematică',icon: '../../assets/images/math.png' },
-    { nume: 'Test 3', scor: 78, durata:'00:15:10', domeniu:'Informatică',icon: '../../assets/images/it.png'  },
-    { nume: 'Test 4', scor: 78, durata:'00:15:10', domeniu:'Istorie',icon: '../../assets/images/history.png'  },
+    {
+      nume: 'Test 1',
+      scor: 85,
+      durata: '00:10:15',
+      domeniu: 'Biologie',
+      icon: '../../assets/images/biology.png',
+    },
+    {
+      nume: 'Test 2',
+      scor: 92,
+      durata: '00:7:20',
+      domeniu: 'Matematică',
+      icon: '../../assets/images/math.png',
+    },
+    {
+      nume: 'Test 3',
+      scor: 78,
+      durata: '00:15:10',
+      domeniu: 'Informatică',
+      icon: '../../assets/images/informatics.png',
+    },
+    {
+      nume: 'Test 4',
+      scor: 78,
+      durata: '00:15:10',
+      domeniu: 'Geografie',
+      icon: '../../assets/images/geography.png',
+    },
   ];
 }
--- frontend/QuizMatrix/src/app/teste-favorite/teste-favorite.component.ts
+++ frontend/QuizMatrix/src/app/teste-favorite/teste-favorite.component.ts
@@ -3,14 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-teste-favorite',
   templateUrl: './teste-favorite.component.html',
-  styleUrl: './teste-favorite.component.css'
+  styleUrl: './teste-favorite.component.css',
 })
 export class TesteFavoriteComponent {
-  
   teste = [
-    { nume: 'Test 1', scor: 85, durata:'00:10:15', domeniu:'Biologie',icon: '../../assets/images/biology.png' },
-    { nume: 'Test 2', scor: 92, durata:'00:7:20', domeniu:'Matematică',icon: '../../assets/images/math.png' },
-    { nume: 'Test 3', scor: 78, durata:'00:15:10', domeniu:'Informatică',icon: '../../assets/images/it.png'  },
-    { nume: 'Test 4', scor: 78, durata:'00:15:10', domeniu:'Istorie',icon: '../../assets/images/history.png'  },
+    {
+      nume: 'Test 1',
+      scor: 85,
+      durata: '00:10:15',
+      domeniu: 'Biologie',
+      icon: '../../assets/images/biology.png',
+    },
+    {
+      nume: 'Test 2',
+      scor: 92,
+      durata: '00:7:20',
+      domeniu: 'Matematică',
+      icon: '../../assets/images/math.png',
+    },
+    {
+      nume: 'Test 3',
+      scor: 78,
+      durata: '00:15:10',
+      domeniu: 'Informatică',
+      icon: '../../assets/images/informatics.png',
+    },
+    {
+      nume: 'Test 4',
+      scor: 78,
+      durata: '00:15:10',
+      domeniu: 'Geografie',
+      icon: '../../assets/images/geography.png',
+    },
   ];
 }
