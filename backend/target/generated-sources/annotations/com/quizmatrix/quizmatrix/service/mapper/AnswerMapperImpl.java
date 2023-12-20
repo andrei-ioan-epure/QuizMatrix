2505628d5619fa41d06e26fcec75e54bfb5c08ca
andrei-ioan-epure <epureandrei567@gmail.com>
2023-12-20 20:34:41 +0200

    Added answer selection and score calculation

--- backend/target/generated-sources/annotations/com/quizmatrix/quizmatrix/service/mapper/AnswerMapperImpl.java
+++ backend/target/generated-sources/annotations/com/quizmatrix/quizmatrix/service/mapper/AnswerMapperImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-18T21:48:23+0200",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+    date = "2023-12-20T19:22:36+0200",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
 public class AnswerMapperImpl implements AnswerMapper {
--- backend/target/generated-sources/annotations/com/quizmatrix/quizmatrix/service/mapper/LeaderBoardMapperImpl.java
+++ backend/target/generated-sources/annotations/com/quizmatrix/quizmatrix/service/mapper/LeaderBoardMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-18T21:48:22+0200",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+    date = "2023-12-20T19:22:36+0200",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
 public class LeaderBoardMapperImpl implements LeaderBoardMapper {
