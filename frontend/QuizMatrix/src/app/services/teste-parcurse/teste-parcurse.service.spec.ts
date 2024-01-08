ae432f5436ea684b42469d054753dd9293240d3e
iuliar16 <iuliana-elena.rusu@student.tuiasi.ro>
2024-01-08 18:06:46 +0200

    Tests for CompletedTest Component and Service

--- frontend/QuizMatrix/src/app/services/teste-parcurse/teste-parcurse.service.spec.ts
+++ frontend/QuizMatrix/src/app/services/teste-parcurse/teste-parcurse.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TesteParcurseService } from './teste-parcurse.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('TesteParcurseService', () => {
   let service: TesteParcurseService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TesteParcurseService]
+    });
+
     service = TestBed.inject(TesteParcurseService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make a GET to obtain completed tests', () => {
+    const id_user = 1;
+    service.getCompletedTests(id_user).subscribe();
+
+    const req = httpTestingController.expectOne(`${service['baseUrl']}/getByIdUser?id_user=${id_user}`);
+    expect(req.request.method).toEqual('GET');
+  });
+
+  it('should make a POST request to add a test to completed tests', () => {
+    const id_quiz = 3;
+    const id_user = 1;
+    const score = 100;
+
+    service.addTestToCompleted(id_quiz, id_user, score).subscribe();
+
+    const req = httpTestingController.expectOne(service['baseUrl']);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual({
+      id_quiz: id_quiz,
+      id_user: id_user,
+      score: score,
+      date_completed: jasmine.any(String)
+    });
+  });
+
+  it('should emit events for added tests', () => {
+    const test = { id: 1, name: 'Test 1' };
+    spyOn(service['testAdaugatLaParcurse'], 'next');
+
+    service.testAdaugat(test);
+
+    expect(service['testAdaugatLaParcurse'].next).toHaveBeenCalledWith(test);
+  });
+
 });
--- frontend/QuizMatrix/src/app/teste-parcurse/teste-parcurse.component.spec.ts
+++ frontend/QuizMatrix/src/app/teste-parcurse/teste-parcurse.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TesteParcurseComponent } from './teste-parcurse.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { StorageService } from '../services/storage/storage.service';
+import { QuizDataService } from '../services/quiz-data/quiz-data.service';
+import { TesteParcurseService } from '../services/teste-parcurse/teste-parcurse.service';
+import { QuizService } from '../services/quizService/quiz.service';
+import { of } from 'rxjs';
 
 describe('TesteParcurseComponent', () => {
   let component: TesteParcurseComponent;
@@ -8,10 +15,14 @@ describe('TesteParcurseComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatIconModule
+      ],
       declarations: [TesteParcurseComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(TesteParcurseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +31,21 @@ describe('TesteParcurseComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize data on ngOnInit', () => {
+    const storageService = TestBed.inject(StorageService);
+    spyOn(storageService, 'isLoggedIn').and.returnValue(true);
+    const quizDataService = TestBed.inject(QuizDataService);
+    spyOn(quizDataService, 'getQuizData').and.returnValue({ quizId: 123, score: 0, totalTimeSpent: 0 });
+    spyOn(quizDataService, 'resetQuizData').and.callThrough();
+    const testeParcurseService = TestBed.inject(TesteParcurseService);
+    spyOn(testeParcurseService, 'getCompletedTests').and.returnValue(of([])); // Presupunem că întoarce un array gol
+
+    component.ngOnInit();
+
+    expect(quizDataService.resetQuizData).toHaveBeenCalled();
+    expect(storageService.isLoggedIn).toHaveBeenCalled();
+    expect(testeParcurseService.getCompletedTests).toHaveBeenCalled();
+  });
+
 });
