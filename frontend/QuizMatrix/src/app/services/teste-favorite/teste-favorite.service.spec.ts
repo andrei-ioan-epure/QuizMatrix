32096e6901ad635d49982606cd7542ca0b4392c4
iuliar16 <iuliana-elena.rusu@student.tuiasi.ro>
2024-01-08 17:45:36 +0200

    Tests for TesteFavorite Component and Service

--- frontend/QuizMatrix/src/app/services/teste-favorite/teste-favorite.service.spec.ts
+++ frontend/QuizMatrix/src/app/services/teste-favorite/teste-favorite.service.spec.ts
@@ -1,16 +1,64 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TesteFavoriteService } from './teste-favorite.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MatIconModule } from '@angular/material/icon';
+
 
 describe('TesteFavoriteService', () => {
   let service: TesteFavoriteService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TesteFavoriteService]
+    });
+
     service = TestBed.inject(TesteFavoriteService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
+  afterEach(() => {
+    httpTestingController.verify(); 
+  });
+  
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make a GET request to obtain favorite tests', () => {
+    const id_user = 1;
+    const testUrl = 'http://localhost:8090/quiz_user/getByIdUser?id_user=' + id_user;
+  
+    service.getTesteFavorite(id_user).subscribe();
+  
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('GET');
+  });
+
+  it('should make a DELETE request to eliminate tests from favorite', () => {
+    const id_quiz = 2;
+    const testUrl = 'http://localhost:8090/quiz_user/delete?id_quiz=' + id_quiz;
+  
+    service.removeTestFromFavorites(id_quiz).subscribe();
+  
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('DELETE');
+  });
+
+  it('should make a POST request to add a test to favorites', () => {
+    const id_quiz = 3;
+    const id_user = 1;
+    const testUrl = 'http://localhost:8090/quiz_user';
+  
+    service.addTestToFavorites(id_quiz, id_user).subscribe();
+  
+    const req = httpTestingController.expectOne(testUrl);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual({ id_quiz: id_quiz, id_user: id_user });
+  });
+  
+  
+
 });
--- frontend/QuizMatrix/src/app/teste-favorite/teste-favorite.component.spec.ts
+++ frontend/QuizMatrix/src/app/teste-favorite/teste-favorite.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TesteFavoriteComponent } from './teste-favorite.component';
+import { MatIconModule } from '@angular/material/icon';
+import { StorageService } from '../services/storage/storage.service';
+import { TesteFavoriteService } from '../services/teste-favorite/teste-favorite.service';
+import { of } from 'rxjs';
 
 describe('TesteFavoriteComponent', () => {
   let component: TesteFavoriteComponent;
@@ -8,6 +12,10 @@ describe('TesteFavoriteComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatIconModule
+      ],
       declarations: [TesteFavoriteComponent]
     })
     .compileComponents();
@@ -20,4 +28,32 @@ describe('TesteFavoriteComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize favorite tests on ngOnInit', () => {
+    const storageService = TestBed.inject(StorageService);
+    spyOn(storageService, 'isLoggedIn').and.returnValue(true);
+    spyOn(storageService, 'getUser').and.returnValue({ id_user: 1 });
+    const testeFavoriteService = TestBed.inject(TesteFavoriteService);
+    spyOn(testeFavoriteService, 'getTesteFavorite').and.returnValue(of([])); 
+  
+    component.ngOnInit();
+  
+    expect(storageService.isLoggedIn).toHaveBeenCalled();
+    expect(storageService.getUser).toHaveBeenCalled();
+    expect(testeFavoriteService.getTesteFavorite).toHaveBeenCalledWith(1);
+  });
+
+  it('should eliminate a test from favorites', () => {
+    const testId = 2; 
+    const testeFavoriteService = TestBed.inject(TesteFavoriteService);
+    spyOn(testeFavoriteService, 'removeTestFromFavorites').and.returnValue(of({})); 
+    component.teste = [{ quiz: { id_quiz: testId } }];
+  
+    component.removeFromFavorites(testId);
+  
+    expect(testeFavoriteService.removeTestFromFavorites).toHaveBeenCalledWith(testId);
+    expect(component.teste.length).toBe(0);
+  });
+
+  
 });
