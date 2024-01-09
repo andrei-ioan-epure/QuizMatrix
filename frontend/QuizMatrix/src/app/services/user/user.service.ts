aa7f06bb567d7429755e8b42851246afaa137cce
Mari <axana-marinela.diaconu@student.tuiasi.ro>
2024-01-09 18:55:19 +0200

    afisara msj daca nu poti sterge userul curent si userii cu istoric

--- frontend/QuizMatrix/src/app/services/user/user.service.ts
+++ frontend/QuizMatrix/src/app/services/user/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { StorageService } from '../storage/storage.service';
+import { catchError, throwError } from 'rxjs';
 
 const API_URL = 'http://localhost:8090/api/auth';
 
@@ -14,12 +15,20 @@ export class UserService {
   getAllUsers(): Observable<any> {
     return this.http.get(`${API_URL}/users`);
   }
-  
 
   deleteUser(userId: number): Observable<any> {
     const url = `${API_URL}/users/${userId}`;
-    return this.http.delete(url);
+    return this.http.delete(url).pipe(
+      catchError((error: any) => {
+        if (
+          error?.error?.message ===
+          'Cannot delete or update a parent row: a foreign key constraint fails'
+        ) {
+          return throwError('Cannot delete user with associated records.');
+        } else {
+          return throwError('Error deleting user.');
+        }
+      })
+    );
   }
-
-  
 }
--- frontend/QuizMatrix/src/app/users/users.component.ts
+++ frontend/QuizMatrix/src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user/user.service';
+import { StorageService } from '../services/storage/storage.service';
 
 @Component({
   selector: 'app-users',
@@ -8,11 +9,17 @@ import { UserService } from '../services/user/user.service';
 })
 export class UsersComponent implements OnInit {
   users: any[] = [];
+  errorMessage: string = '';
+  currentUser: any;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private storageService: StorageService
+  ) {}
 
   ngOnInit(): void {
     this.loadUsers();
+    this.currentUser = this.storageService.getUser();
   }
 
   loadUsers(): void {
@@ -20,9 +27,11 @@ export class UsersComponent implements OnInit {
       (data: any) => {
         console.log('Loaded users:', data);
         this.users = data;
+        this.errorMessage = '';
       },
       (error) => {
         console.error('Error loading users:', error);
+        this.errorMessage = 'Eroare la încărcarea utilizatorilor.';
       }
     );
   }
@@ -31,6 +40,18 @@ export class UsersComponent implements OnInit {
     console.log('Deleting user with ID:', userId);
 
     if (userId) {
+      // Verificăm dacă utilizatorul curent încearcă să se șteargă pe el însuși
+      if (userId === this.currentUser.id_user) {
+        this.errorMessage = 'Nu poți șterge utilizatorul curent.';
+        return;
+      }
+
+      // Verificăm dacă utilizatorul curent are drepturi de "ADMIN" pentru a permite ștergerea
+      if (!this.storageService.isUserAdmin()) {
+        this.errorMessage = 'Nu ai drepturi pentru a șterge utilizatori.';
+        return;
+      }
+
       this.userService.deleteUser(userId).subscribe(
         () => {
           console.log('User deleted successfully.');
@@ -38,6 +59,11 @@ export class UsersComponent implements OnInit {
         },
         (error) => {
           console.error('Error deleting user:', error);
+          if (error === 'Cannot delete user with associated records.') {
+            this.errorMessage = 'Cannot delete user with associated records.';
+          } else {
+            this.errorMessage = 'Nu se poate șterge un user cu istoric.';
+          }
         }
       );
     } else {
